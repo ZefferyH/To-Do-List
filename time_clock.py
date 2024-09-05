ab54fdc0574b7a337e7acf0f689ea071f56c400f
Zi Xu Huang <155401941+ZefferyH@users.noreply.github.com>
2024-09-05 10:25:26 -0400

    Update time_clock.py
    fixed a bug in time calculation for past due tasks

--- time_clock.py
+++ time_clock.py
@@ -16,20 +16,22 @@ def time_difference(time1):
     current_time_struct = time.localtime()
     time1_seconds = time.mktime(time1_struct)
     current_time_seconds = time.mktime(current_time_struct)
-    difference_seconds = current_time_seconds - time1_seconds
-    days = - 1 * difference_seconds // (24 * 3600) + 1
-    difference_seconds = difference_seconds % (24 * 3600)
-    hours = difference_seconds // 3600
-    difference_seconds %= 3600
-    minutes = difference_seconds // 60
-    seconds = difference_seconds % 60
+    difference_seconds = (current_time_seconds - time1_seconds)
+    is_in_future = difference_seconds <= 0
+    if is_in_future:
+        difference_seconds = difference_seconds * -1
+    days = difference_seconds // (24 * 3600)
+    difference_seconds_in_day = difference_seconds % (24 * 3600)
+    hours = difference_seconds_in_day // 3600
+    difference_seconds_in_hour = difference_seconds_in_day % 3600
+    minutes = difference_seconds_in_hour // 60
 
 
     return {
         'days': int(days),
         'hours': int(hours),
         'minutes': int(minutes),
-        'seconds': int(seconds)
+        'in_future':is_in_future
     }
 
 def task_to_mktime(task):
